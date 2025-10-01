434ac8e4cdf393b458a42003566ac989c495a57c
rehman tyagi <irontyagi4@gmail.com>
2025-10-01 23:21:04 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import React from 'react'
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <h1>hello device</h1>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="text-red-500 font-bold text-xl text-center">
+      header component
+    </div>
+  )
+}
+
+export default Header
